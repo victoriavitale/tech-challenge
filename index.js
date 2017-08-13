b50ca5c26e1c99b6676161b26a62f4ccd694e07e
Maria Victoria Vitale <Vi@Marias-MacBook-Pro.local>
2017-08-13 11:47:51 -0300

    add cors

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express'),
       mongoose = require('mongoose'),
       routes = require('./routes'),
-      userModel = require('./models/users');
-
+      userModel = require('./models/users'),
+      cors = require('cors');
+      
 const app = express();
 
 mongoose.connect('mongodb://localhost/contacts-app');
 
+app.use(cors());
 app.use('/', routes);
 
-app.listen(3000, function(){
-    console.log("Server has started on port 3000")
+app.listen(5000, function(){
+    console.log("Server has started on port 5000")
 })
 
